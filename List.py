6d9c3fb7fce3b9ee6872d2fc43f76de3322cd615
Wizat <passavit1@gmail.com>
2023-05-30 17:39:20 +0700

    Learn more

--- List.py
+++ List.py
@@ -25,5 +25,18 @@ print(myList)
 # myList.pop()
 # print(myList)
 
-for x in myList:
-    print(x)
+# newList = [expression for item in iterable if condition == true]
+newList = [x for x in myList if 'm' in x]
+print('เลือกเฉพาะที่มี m ', newList)
+
+newList = [x for x in myList if 'My' not in x]
+print('เลือกทุกตัวยกเวิน my ', newList)
+
+newList = [x.upper() for x in myList]
+print('ปรับทุกตัวเป้น uppercase  ', newList)
+
+newList = ['hello' for x in myList]
+print('เปลี่ยนทุก index เป็น hello ', newList)
+
+newListOfNumber = [x for x in range(10) if x < 5]
+print(newListOfNumber)
