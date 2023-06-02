94360b9c13655ea765fa52c7f2f9c759093824c6
Wizat <passavit1@gmail.com>
2023-06-02 16:43:07 +0700

    Learn w3 till sort Lists

--- List.py
+++ List.py
@@ -26,17 +26,23 @@ print(myList)
 # print(myList)
 
 # newList = [expression for item in iterable if condition == true]
-newList = [x for x in myList if 'm' in x]
-print('เลือกเฉพาะที่มี m ', newList)
+# newList = [x for x in myList if 'm' in x]
+# print('เลือกเฉพาะที่มี m ', newList)
 
-newList = [x for x in myList if 'My' not in x]
-print('เลือกทุกตัวยกเวิน my ', newList)
+# newList = [x for x in myList if 'My' not in x]
+# print('เลือกทุกตัวยกเวิน my ', newList)
 
-newList = [x.upper() for x in myList]
-print('ปรับทุกตัวเป้น uppercase  ', newList)
+# newList = [x.upper() for x in myList]
+# print('ปรับทุกตัวเป้น uppercase  ', newList)
 
-newList = ['hello' for x in myList]
-print('เปลี่ยนทุก index เป็น hello ', newList)
+# newList = ['hello' for x in myList]
+# print('เปลี่ยนทุก index เป็น hello ', newList)
 
-newListOfNumber = [x for x in range(10) if x < 5]
-print(newListOfNumber)
+# newListOfNumber = [x for x in range(10) if x < 5]
+# print(newListOfNumber)
+
+myList.sort()
+print(myList, 'sort list ')
+
+myList.sort(reverse=True)
+print(myList, 'sort list reverse ')
